748b48aca6b5b79747a71b29f2afb94e3158358b
Ali <tuf22191@temple.edu>
2016-01-26 22:47:01 +0000

    I read online that I should have a virtual environment replica.

---
+++ rangoVirtualEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\muhammadayub\documents\coding_in_general\python\virt_env\rangovirtualenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
